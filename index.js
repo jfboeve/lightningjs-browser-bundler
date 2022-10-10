924a2500e76b40dca6583e86f0beb1a3c7ca921d
jfboeve <jfboeve@gmail.com>
2022-10-10 16:51:15 +0200

    remove export

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 export { default as ViewCompiler } from './src/ViewCompiler.js';
 export { default as Bundler } from './src/Bundler.js';
-export { default as BundleManager } from './src/BundleManager.js';
